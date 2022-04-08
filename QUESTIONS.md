0bca51d7dbcefdac3231f46b3088738c8342fed4
恪晨 <bo.wang1016@outlook.com>
2022-04-08 18:18:33 +0800

    change frame

---
+++ QUESTIONS.md
@@ -0,0 +1,5 @@
+## QUESTIONS
+
+1、pages下面的文件夹，虽然文件夹名称不同，但是如果里面都是index.tsx则会报错：Please do not register multiple Pages in pages/index/index.js；
+
+2、app.json中pages的配置，index必须在最下面，不然tabBar不生效；如果使用UI组件库，则index必须在最上面；
--- miniprogram/pages/about/index.ts
+++ miniprogram/pages/about/index.ts
@@ -1,26 +1,19 @@
-// index.ts
-// 获取应用实例
-const app = getApp<IAppOption>()
-
+// pages/about/about.ts
 Page({
+
+  /**
+   * 页面的初始数据
+   */
   data: {
-    active: 0,
-    motto: 'Hello World',
     userInfo: {},
     hasUserInfo: false,
     canIUse: wx.canIUse('button.open-type.getUserInfo'),
     canIUseGetUserProfile: false,
     canIUseOpenData: wx.canIUse('open-data.type.userAvatarUrl') && wx.canIUse('open-data.type.userNickName') // 如需尝试获取用户信息可改为false
   },
-  onChange(event: { detail: any; }) {
-    // event.detail 的值为当前选中项的索引
-    this.setData({ active: event.detail });
-  },
   // 事件处理函数
   bindViewTap() {
-    wx.navigateTo({
-      url: '../home/index',
-    })
+
   },
   onLoad() {
     // @ts-ignore
@@ -50,5 +43,61 @@ Page({
       userInfo: e.detail.userInfo,
       hasUserInfo: true
     })
+  },
+
+  /**
+   * 生命周期函数--监听页面加载
+   */
+  onLoad() {
+
+  },
+
+  /**
+   * 生命周期函数--监听页面初次渲染完成
+   */
+  onReady() {
+
+  },
+
+  /**
+   * 生命周期函数--监听页面显示
+   */
+  onShow() {
+    this.getTabBar().init()
+  },
+
+  /**
+   * 生命周期函数--监听页面隐藏
+   */
+  onHide() {
+
+  },
+
+  /**
+   * 生命周期函数--监听页面卸载
+   */
+  onUnload() {
+
+  },
+
+  /**
+   * 页面相关事件处理函数--监听用户下拉动作
+   */
+  onPullDownRefresh() {
+
+  },
+
+  /**
+   * 页面上拉触底事件的处理函数
+   */
+  onReachBottom() {
+
+  },
+
+  /**
+   * 用户点击右上角分享
+   */
+  onShareAppMessage() {
+
   }
 })
