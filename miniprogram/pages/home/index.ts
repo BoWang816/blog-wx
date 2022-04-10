dff211c1bdf735922a01cd74669f65c5ed711f3d
恪晨 <bo.wang1016@outlook.com>
2022-04-10 14:58:23 +0800

    create home page

--- miniprogram/pages/home/index.ts
+++ miniprogram/pages/home/index.ts
@@ -1,18 +1,47 @@
 // pages/about/about.ts
+import {API_DOMAIN} from '../../constants/index';
 Page({
 
   /**
    * 页面的初始数据
    */
   data: {
-
+    // 文章信息
+    articleData: {
+      total: 0,
+      pageCount: 1,
+      pageSize: 10,
+      data: []
+    },
+    // 搜索框内容
+    searchValue: '',
+    // 轮播信息
+    swiperList: []
   },
 
   /**
    * 生命周期函数--监听页面加载
    */
   onLoad() {
-
+    // 获取文章列表
+    let that = this;
+    wx.request({
+      url: `${API_DOMAIN}posts.json`,
+      success(res) {
+            const {data, pageCount, pageSize, total} = res.data;
+            that.setData({
+                articleData: {
+                    data,
+                    pageCount,
+                    pageSize,
+                    total
+                },
+                swiperList: data.filter((i, index) => index <= 4),
+                noticeContent: '公告信息'
+            });
+            console.log(data);
+        }
+    });
   },
 
   /**
@@ -62,5 +91,17 @@ Page({
    */
   onShareAppMessage() {
 
+  },
+
+  onSearch(e: any) {
+    this.setData({
+      searchValue: e.detail
+    });
+},
+
+  onCancelSearch() {
+    this.setData({
+      searchValue: ''
+    });
   }
 })
